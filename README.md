a599162b631d848c18318dc48bf58c0c39778688
romeo mureithi <romeomureithi@gmail.com>
2021-12-25 03:25:37 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#New Year's Countdown Timer
---
+++ view.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, redirect
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/alert')
+def success():
+    return render_template('alert.html')
+if __name__ == '__main__':
+    #from waitress import serve
+    app.run(use_reloader=True, debug=True)
+    #serve(app, host="localhost", port=5000)
